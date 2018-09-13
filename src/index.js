9d2667744b52a5c99bf427c20282894908d7719d
Michael Himing <mhiming@gmail.com>
2018-09-13 19:02:24 +1000

    Fix newlines

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const $ = document.querySelector.bind(document);
 
 function onInput() {
     const source = $('#input').value;
-    let output = source.replace(/../g, s => String.fromCharCode(s.charCodeAt(0) * 256 + s.charCodeAt(1)));
+    let output = source.replace(/[\s\S]{2}/g, s => String.fromCharCode(s.charCodeAt(0) * 256 + s.charCodeAt(1)));
     if (output.charAt(output.length - 1) === '`') {
         output = output.substr(0, output.length - 1) + '\\`';
     }
