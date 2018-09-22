d6ccdb8445d695b50c1381a062a4b9a2c4496da7
Michael Himing <mhiming@gmail.com>
2018-09-22 13:22:24 +1000

    Add xem's 2 char and Roman's 3 char methods (still needs a mode selector to use the latter)

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function onInput() {
         if (decompressMatch) {
             output = eval(decompressMatch[1]);
         } else {
-            output = compress(source);
+            output = compressUnicode2(source);
         }
         $('#output').textContent = output;
         $('#output-count').textContent = `${output.length} chars`;
@@ -23,18 +23,58 @@ function onInput() {
     }
 }
 
-function compress(input) {
+// http://xem.github.io/golfing/#jstweet_en
+function compressUnicode2(input) {
     for (const char of input) {
         if (char.charCodeAt(0) > 255) {
-            throw "Input must contain only ascii characters";
+            throw 'Input must contain only characters 0-255';
         }
     }
-    const pattern = /[\s\S]{2}/g; // matches any two characters, including newlines
-    let output = input.replace(pattern, s => String.fromCharCode(s.charCodeAt(0) * 256 + s.charCodeAt(1)));
-    if (output.charAt(output.length - 1) === '`') {
-        output = output.substr(0, output.length - 1) + '\\`';
+    // pad with a space to get an even length
+    if (input.length % 2 === 1) {
+        input += ' ';
     }
-    return 'eval(unescape(escape`' + output + '`.replace(/u(..)/g,"$1%")))';
+    let output = '';
+    for (let i = 0; i < input.length; i += 2) {
+        output += String.fromCharCode(0xD800 + input.charCodeAt(i));
+        output += String.fromCharCode(0xDC00 + input.charCodeAt(i + 1));
+    }
+    if (unescape(escape(output).replace(/uD./g, '')) !== input) {
+        throw 'Failed to compress';
+    }
+    return `eval(unescape(escape\`${output}\`.replace(/uD./g,'')))`;
+}
+
+// http://www.romancortes.com/v2/javascript-code-golf.html
+function compressUnicode3(input) {
+    for (const char of input) {
+        const charCode = char.charCodeAt(0);
+        if (charCode < 32 || charCode > 127) {
+            throw 'Input must contain only characters 32-127';
+        }
+    }
+    if (!input.startsWith(';')) {
+        input = ';' + input;
+    }
+    while (input.length % 3 !== 0) {
+        input += ' ';
+    }
+    let output = '';
+    for (let i = 0; i < input.length; i += 3) {
+        output += String.fromCodePoint(input.charCodeAt(i) - 31 +
+            (input.charCodeAt(i + 1) - 31) * 97 +
+            (input.charCodeAt(i + 2) - 31) * 97 * 97);
+    }
+    let test = '';
+    for (const c of output) {
+        for (let v = c.codePointAt(0); v | 0; v /= 97) {
+            test += String.fromCharCode(v % 97 + 31);
+        }
+    }
+    if (test !== input) {
+        throw 'Failed to compress';
+    }
+    return `_=0;for(c of'${output}')for(v=c.codePointAt();v|0;v/=97)_+=String.fromCharCode(v%97+31);eval(_)`;
 }
 
 $('#input').addEventListener('input', onInput);
